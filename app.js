3c50d3f8a1c64118d78cf1b56bc6956fb9a0e68c
Marco Whyte <mwhyte@umass.edu>
2020-10-08 18:14:54 -0500

    html ionic app calculator finished

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-const calculateBtn = document.querySelector("ion-button");
+const calculateBtn = document.getElementById("calc-btn");
+const resetBtn = document.getElementById("reset");
 
 const heightInput = document.getElementById("height");
 const weightInput = document.getElementById("weight");
+
+const resultArea = document.getElementById("result");
+
+const resetInputs = () => {
+  heightInput.value = "";
+  weightInput.value = "";
+  resultArea.innerHTML = "";
+};
 
 const calculateBMI = () => {
   const enteredHeight = +heightInput.value;
   const enteredWeight = +weightInput.value;
 
   const bmi = enteredWeight / (enteredHeight * enteredHeight);
+  if (isNaN(bmi)) {
+    return;
+  }
+  const resultElement = document.createElement("ion-card");
 
-  console.log(bmi);
+  resultElement.innerHTML = `
+  <ion-card-content><h2>${bmi}</h2></ion-card-content>`;
+  resultArea.innerHTML = "";
+  resultArea.appendChild(resultElement);
 };
 
 calculateBtn.addEventListener("click", calculateBMI);
+
+resetBtn.addEventListener("click", resetInputs);
